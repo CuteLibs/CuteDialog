b0803a6be6d6b25548c8f0224e2203dd4dca8568
ahmmedrejowan <ahmmedrejowan@gmail.com>
2022-09-17 21:53:54 +0600

    Major Changes in Code

---
+++ app/src/main/java/io/github/cutelibs/cutedialogdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package io.github.cutelibs.cutedialogdemo;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.github.cutelibs.cutedialog.CuteDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v -> {
+
+            new CuteDialog(this)
+                    .show();
+
+        });
+
+
+    }
+}
